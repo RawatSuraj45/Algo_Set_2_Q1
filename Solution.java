59b0b5010268b3edfcb4f9569e559cf9a4ec19c1
RawatSuraj45 <156521323+RawatSuraj45@users.noreply.github.com>
2024-08-03 12:50:01 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,20 +4,20 @@ class Main {
     length = arr1.length();
     int [] arr2 = {7 , 6 , 4 , 3 , 1};
     length2 = arr2.length();
-    System.out.println(maxProfit(arr1 , length));
-    System.out.println(maxProfit(arr2 , length2));
+    System.out.println(profit(arr1 , length));
+    System.out.println(profit(arr2 , length2));
   }
-  static int maxProfit( int arr[] , int len){
+  static int profit( int arr[] , int len){
     int buy = arr[0] ;
-    int maxprofit = 0 ;
+    int Profit = 0 ;
     for (int i = 0 ; i < len - 1 ; i++){
       if(buy > arr[i]){
         buy = arr[i];
       }
       elseif(arr[i] - buy > maxprofit){
-        maxprofit = arr[i] - buy;
+        Profit = arr[i] - buy;
       }
-      return maxprofit ; 
+      return Profit ; 
     }
   }
 }
